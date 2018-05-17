e016e2a2b4efb97681d5a99ca9ab3a4162e9cb85
Sarah Azouvi <sazouvi@eduroam-int-dhcp-97-3-123.ucl.ac.uk>
2018-05-17 20:10:33 +0100

    scp

--- scp.sh
+++ scp.sh
@@ -1,1 +1,3 @@
 scp -r publication.html sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/publication.html
+scp -r papers/fantomette.pdf sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/papers/fantomette.pdf
+scp -r slides/fantomette-slides.pdf sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/slides/fantomette-slides.pdf
