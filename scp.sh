0168aaefe5689aff6f7c6a836827ec9bab006bad
Sarah Azouvi <sarah8@bitbucket.org>
2019-08-18 21:02:03 +0100

    added sok and job search

--- scp.sh
+++ scp.sh
@@ -1,3 +1,5 @@
 scp -r publication.html sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/publication.html
+scp -r index.html sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/index.html
 scp -r papers/fantomette.pdf sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/papers/fantomette.pdf
 scp -r slides/fantomette-slides.pdf sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/slides/fantomette-slides.pdf
+scp -r SA_01.jpg sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/SA_01.jpg
