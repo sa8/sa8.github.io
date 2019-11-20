ca70b24de9040cdb5742933cf7173266ab2c91f0
Sarah Azouvi <sarah8@bitbucket.org>
2019-11-20 15:11:15 +0000

    removed looking for a job

--- scp.sh
+++ scp.sh
@@ -1,6 +1,6 @@
 scp -r publication.html sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/publication.html
 scp -r index.html sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/index.html
 scp -r papers/fantomette.pdf sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/papers/fantomette.pdf
-scp -r slides/fantomette-slides.pdf sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/slides/fantomette-slides.pdf
+scp -r slides/talk-epfl.pdf sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/slides/talk-epfl.pdf
 scp -r SA_01.jpg sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/SA_01.jpg
 scp -r cv.pdf sazouvi@storm.cs.ucl.ac.uk:/cs/academic/phd3/sazouvi/public_html/cv.pdf
